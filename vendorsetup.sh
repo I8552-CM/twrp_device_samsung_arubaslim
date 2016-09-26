05ca56f024337178a0865bf2e00e30c10a913776
Grace5921 <androidlover5842@gmail.com>
2016-09-26 12:09:59 +0530

    typo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,8 @@
 add_lunch_combo omni_arubaslim-userdebug
-device=arubaslim
 function mkazip_arubaslim
 {
-cp device/samsung/$device/META-INF.zip out/target/product/$device/META-INF.zip 
-cd out/target/product/$device/
+cp device/samsung/arubaslim/META-INF.zip out/target/product/arubaslim/META-INF.zip 
+cd out/target/product/arubaslim/
 unzip META-INF.zip 
 rm META-INF.zip 
 zip twrp-$(date +%Y%m%d)-3.0.0-UNOFFICIAL-arubaslim.zip  -r recovery.img -r META-INF
